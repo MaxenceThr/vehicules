895788efcc13c02e053080f26dc0407c97fa53b9
Maxence THERON <theronmaxence@gmail.com>
2025-11-17 00:07:55 +0100

    miseAjour

--- TestVehicules.java
+++ TestVehicules.java
@@ -2,7 +2,14 @@ package vehicules;
 class TestVehicules{
 
     public  static  void main(String[] args){
-    
-}
+        /*
+		Vehicule v1 = new Vehicule(35.5, 1352);
+		System.out.println(v1);
+		
+		v1 = new Vehicule(35.5, -2);
+		System.out.println(v1);
+		
+		*/
+    }
 
 }
